52e9b3f8f4a10f67ca4762d78842301d80145965
Alexis Manosca <alexismanosca@gmail.com>
2026-03-05 21:13:31 +1300

    created a test file

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        StrMath strMath = new StrMath();
+        
+        System.out.println(strMath.StrAddDigit("6", "7"));
+    }
+}
