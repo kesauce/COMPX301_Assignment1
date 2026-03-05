5843a77af4d946acb4d32c53a2eb1f7dd2293c81
Alexis Manosca <alexismanosca@gmail.com>
2026-03-05 20:13:33 +1300

    initial commit

---
+++ StrMath.java
@@ -0,0 +1,13 @@
+class StrMath{
+
+    public String StrAdd(String input1, String input2){
+        return "";
+    }
+
+    public String StrMult(String input1, String input2){
+        return "";
+    }
+    public String StrFacr(String input){
+        return "";
+    }
+}
