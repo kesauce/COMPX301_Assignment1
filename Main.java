d949a63414ec6f05a25a6c679379524d1226a1a8
Alexis Manosca <alexismanosca@gmail.com>
2026-03-06 15:27:39 +1300

    added a multiplication table and an multiply single digit method

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         StrMath strMath = new StrMath();
         
-        //System.out.println(strMath.StrAddDigit("7", "7"));
+        System.out.println(strMath.StrMultDigit("7", "7"));
         System.out.println(strMath.StrAdd("7999", "1"));
     }
 }
--- StrMath.java
+++ StrMath.java
@@ -23,11 +23,25 @@ class StrMath {
             { { "0", "9" }, { "1", "0" }, { "1", "1" }, { "1", "2" }, { "1", "3" }, { "1", "4" }, { "1", "5" }, { "1", "6" }, { "1", "7" }, { "1", "8" } }, // 9
     };
 
+    private final String[][][] MULTIPLICATION_TABLE = {
+           //     0              1             2             3             4             5             6             7             8             9
+            { { "0", "0" }, { "0", "0" }, { "0", "0" }, { "0", "0" }, { "0", "0" }, { "0", "0" }, { "0", "0" }, { "0", "0" }, { "0", "0" }, { "0", "0" } }, // 0
+            { { "0", "0" }, { "0", "1" }, { "0", "2" }, { "0", "3" }, { "0", "4" }, { "0", "5" }, { "0", "6" }, { "0", "7" }, { "0", "8" }, { "0", "9" } }, // 1
+            { { "0", "0" }, { "0", "2" }, { "0", "4" }, { "0", "6" }, { "0", "8" }, { "1", "0" }, { "1", "2" }, { "1", "4" }, { "1", "6" }, { "1", "8" } }, // 2
+            { { "0", "0" }, { "0", "3" }, { "0", "6" }, { "0", "9" }, { "1", "2" }, { "1", "5" }, { "1", "8" }, { "2", "1" }, { "2", "4" }, { "2", "7" } }, // 3
+            { { "0", "0" }, { "0", "4" }, { "0", "8" }, { "1", "2" }, { "1", "6" }, { "2", "0" }, { "2", "4" }, { "2", "8" }, { "3", "2" }, { "3", "6" } }, // 4
+            { { "0", "0" }, { "0", "5" }, { "1", "0" }, { "1", "5" }, { "2", "0" }, { "2", "5" }, { "3", "0" }, { "3", "5" }, { "4", "0" }, { "4", "5" } }, // 5
+            { { "0", "0" }, { "0", "6" }, { "1", "2" }, { "1", "8" }, { "2", "4" }, { "3", "0" }, { "3", "6" }, { "4", "2" }, { "4", "8" }, { "5", "4" } }, // 6
+            { { "0", "0" }, { "0", "7" }, { "1", "4" }, { "2", "1" }, { "2", "8" }, { "3", "5" }, { "4", "2" }, { "4", "9" }, { "5", "6" }, { "6", "3" } }, // 7
+            { { "0", "0" }, { "0", "8" }, { "1", "6" }, { "2", "4" }, { "3", "2" }, { "4", "0" }, { "4", "8" }, { "5", "6" }, { "6", "4" }, { "7", "2" } }, // 8
+            { { "0", "0" }, { "0", "9" }, { "1", "8" }, { "2", "7" }, { "3", "6" }, { "4", "5" }, { "5", "4" }, { "6", "3" }, { "7", "2" }, { "8", "1" } }, // 9
+    };
+
     /**
      * Adds a 1-digit number with another 1-digit number
      * @param firstInput First digit
      * @param secondInput Second digit
-     * @return An array of the answer split into 2 digits.
+     * @return An string of the answer
      */
     public String StrAddDigit(String firstInput, String secondInput){
         // Strip the inputs
@@ -49,6 +63,38 @@ class StrMath {
         }
     }
 
+    /**
+     * Multiplies two single digit numbers
+     * @param firstInput First digit
+     * @param secondInput Second digit
+     * @return A string of the answer
+     */
+    public String StrMultDigit(String firstInput, String secondInput){
+        // Strip the inputs
+        String[] firstInputArray = firstInput.split("");
+        String[] secondInputArray = secondInput.split("");
+
+        // Ensure the inputs are only 1 digit
+        if (firstInputArray.length == 1 && secondInputArray.length == 1){
+            // Grab the answer by using the addition table and the inputs as coordinates
+            String[] answer = MULTIPLICATION_TABLE[DIGITS.indexOf(firstInputArray[0])][DIGITS.indexOf(secondInputArray[0])];
+
+            // Format the answer into a string
+            return String.join("", answer);
+        }
+    
+        // Inputs are not single digits.
+        else{
+            return null;
+        }
+    }
+
+    /**
+     * Adds any positive number
+     * @param firstInput First number
+     * @param secondInput Second number
+     * @return A string of the answer
+     */
     public String StrAdd(String firstInput, String secondInput) {
         // Check which string is shorter and fill it with zero
         String[] firstInputArray = firstInput.split("");
